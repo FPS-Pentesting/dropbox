6ba53df0ad7d03adc3f2a8af1c7f217cced091b1
jetbennett <wolleytej@gmail.com>
2023-09-13 19:11:17 -0400

    Update xrdp_performance_tweaks.sh
    Changed how desktop background gets set:
    "xconf-query" wasn't working well in a script. Instead, the script just directly changes the XML config files using `sed`.
    
    Fixed a bug with the XRDP max_bpp sed command that was making multiple max_bpp lines by inserted if a user ran the script multiple times. This caused the ini file to become invalid, which would make it impossible to start the xrdp service.

--- xrdp_performance_tweaks.sh
+++ xrdp_performance_tweaks.sh
@@ -1,51 +1,43 @@
-# Use this to monitor what changes need to be made: 
-## xfconf-query -c xfce4-desktop -m 
-### Example output
-# set: /backdrop/screen0/monitorVirtual1/workspace0/image-style
-# set: /backdrop/screen0/monitorVirtual1/workspace0/color-style
-# set: /backdrop/screen0/monitorVirtual1/workspace0/rgba1
-# Use this to make changes:
-## xfconf-query -c xfce4-desktop -p insert_property_here -s path/image
+#!/bin/bash
 
-# You can manually toggle these settings via "Desktop"
-## Set image to None
-xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitorrdp0/workspace0/image-style -s 0
-## Set color-style to Solid
-xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitorrdp0/workspace0/color-style -s 0
-## Set color to Black (may not work, the variable isn't created until the user selects a value in the GUI)
-xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitorrdp0/workspace0/rgba1 -n -t int -t int -t int -t int -s 0 -s 0 -s 0 -s 1
+### Setup Desktop Background
+XFCE_CONFIG_DIR="/home/pentest/.config/xfce4/xfconf/xfce-perchannel-xml"
+# Set image to None
+sed -i 's/name="image-style" type="int" value="5"/name="image-style" type="int" value="0"/g' $XFCE_CONFIG_DIR/xfce4-desktop.xml
+# Set color-style to Solid
+sed -i 's/name="color-style" type="int" value="1"/name="color-style" type="int" value="0"/g' $XFCE_CONFIG_DIR/xfce4-desktop.xml
 
-
-# Turn off Windows Compositing (fancy GUI window graphics that seem to really hurt xrdp performance)
+        
+### Turn off Windows Compositing (fancy GUI window graphics that seem to really hurt xrdp performance)
 ## As seen here: https://github.com/neutrinolabs/xrdp/issues/1600
 # You can toggle this setting manually via "Windows Manager Tweaks" -> Compositor
-xfconf-query -c xfwm4 -p /general/use_compositing -t bool -s false
+sed -i 's/name="use_compositing" type="bool" value="true"/name="use_compositing" type="bool" value="false"/' $XFCE_CONFIG_DIR/xfwm4.xml
 
 
 ### Make XRDP Config Changes ###
-CONFIG_FILE='/etc/xrdp/xrdp.ini'
+XRDP_CONFIG='/etc/xrdp/xrdp.ini'
 # Backup old config
-cp -p "$CONFIG_FILE" "$CONFIG_FILE.orig.`date \"+%Y%m%d_%H%M%S\"`"
+cp -p "$XRDP_CONFIG" "$XRDP_CONFIG.orig.`date \"+%Y%m%d_%H%M%S\"`"
 # Increase xrdp TCP send buffer (https://github.com/neutrinolabs/xrdp/issues/1483)
-grep -qxF "tcp_send_buffer_bytes=4194304" $CONFIG_FILE || sed -i -e '/#tcp_send_buffer_bytes=/a\' -e 'tcp_send_buffer_bytes=4194304' $CONFIG_FILE
+grep -qxF "tcp_send_buffer_bytes=4194304" $XRDP_CONFIG || sed -i -e '/#tcp_send_buffer_bytes=/a\' -e 'tcp_send_buffer_bytes=4194304' $XRDP_CONFIG
 # Change from 32-bit to 8-bit color (15-bit works okay, except for with complex images, ex browsing webpages)
-sed -i 's/^max_bpp=/#&/' $CONFIG_FILE
-grep -qxF "max_bpp=8" $CONFIG_FILE || sed -i -e '/#max_bpp=/a\' -e 'max_bpp=8' $CONFIG_FILE
+sed -i 's/^max_bpp=32/#&/' $XRDP_CONFIG
+grep -qxF "max_bpp=8" $XRDP_CONFIG || sed -i -e '/#max_bpp=32/a\' -e 'max_bpp=8' $XRDP_CONFIG
 # Change listening port (responder wants port 3389)
-sed -i 's/^port=3389/#&/' $CONFIG_FILE
-grep -qxF "port=8933" $CONFIG_FILE || sed -i -e '/^#port=3389/a\' -e 'port=8933' $CONFIG_FILE
+sed -i 's/^port=3389/#&/' $XRDP_CONFIG
+grep -qxF "port=8933" $XRDP_CONFIG || sed -i -e '/^#port=3389/a\' -e 'port=8933' $XRDP_CONFIG
 
 
-# Update Network Kernel Send Buffer to be twice that of XRDP's
+### Update Network Kernel Send Buffer to be twice that of XRDP's
 ## These commands can check current value and temporarily change it
 ## sysctl -n net.core.wmem_max
 ## sysctl -w net.core.wmem_max=8388608
-SYSCTL='/etc/sysctl.conf'
+SYSCTL_CONFIG='/etc/sysctl.conf'
 # Backup old config
-cp -p "$SYSCTL" "$SYSCTL.orig.`date \"+%Y%m%d_%H%M%S\"`"
+cp -p "$SYSCTL_CONFIG" "$SYSCTL_CONFIG.orig.`date \"+%Y%m%d_%H%M%S\"`"
 # Make permanent changes to the configuration
-grep -qxF "# Send Buffer Increased Size" $SYSCTL || echo "# Send Buffer Increased Size" >> $SYSCTL
-grep -qxF "net.core.wmem_max=8388608" $SYSCTL || echo "net.core.wmem_max=8388608" >> $SYSCTL
+grep -qxF "# Send Buffer Increased Size" $SYSCTL_CONFIG || echo "# Send Buffer Increased Size" >> $SYSCTL_CONFIG
+grep -qxF "net.core.wmem_max=8388608" $SYSCTL_CONFIG || echo "net.core.wmem_max=8388608" >> $SYSCTL_CONFIG
 
 
 ### Change Terminal Theme and Transparency
@@ -53,7 +45,6 @@ QTERM_CONFIG=/home/pentest/.config/qterminal.org/qterminal.ini
 sed -i 's/TerminalTransparency=.*/TerminalTransparency=0/' $QTERM_CONFIG
 sed -i 's/ApplicationTransparency=.*/ApplicationTransparency=0/' $QTERM_CONFIG
 sed -i 's/colorScheme=.*/colorScheme=WhiteOnBlack/' $QTERM_CONFIG
-
 
 
 ### Get rid of the "Authentication Required to Create Managed Color Device" message
