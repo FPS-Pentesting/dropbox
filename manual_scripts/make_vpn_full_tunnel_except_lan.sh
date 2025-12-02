0bfce8072aea0f582b5fdcda2cfdd0e761f5aeaf
jetbennett <wolleytej@gmail.com>
2025-12-02 17:01:56 -0500

    Update make_vpn_full_tunnel_except_lan.sh
    Added a reset to default state to the top of the file, and clarified error message.

--- manual_scripts/make_vpn_full_tunnel_except_lan.sh
+++ manual_scripts/make_vpn_full_tunnel_except_lan.sh
@@ -5,8 +5,19 @@
 # If you need to hit other client subnets, you must add routing for them
 # This is the default setup for a new NAB
 
+# Reset to Clean/Default State
+sudo ip route del 0.0.0.0/1
+sudo ip route del 128.0.0.0/1
+sudo ip route del 10.0.0.0/8
+sudo ip route del 172.16.0.0/12
+sudo ip route del 192.168.0.0/16
+
+# Route traffic through VPN tunnel by default
 sudo ip route add 128.0.0.0/1 via 10.8.0.1
 sudo ip route add 0.0.0.0/1 via 10.8.0.1
 
+# There should already be a rule to allow eth0 traffic to not be tunneled
+# Ex.: 10.50.0.0/24 dev eth0 proto kernel scope link src 10.50.0.37 metric 100
+
 echo ""
-echo "Errors that say 'RTNETLINK answers: File exists' are okay. That just means a route already existed"
+echo "Errors that say 'File exists/No such process' are okay. That just means a route change was redundant."
