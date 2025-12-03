f55ab402766709cdbc1f25374a963fc2975f4875
jetbennett <wolleytej@gmail.com>
2025-12-03 11:21:45 -0500

    Update make_vpn_subnet_only.sh
    Updated with variable, and now using `dev tun0` instead of hardcoded VPN server IP.

--- manual_scripts/make_vpn_subnet_only.sh
+++ manual_scripts/make_vpn_subnet_only.sh
@@ -7,14 +7,14 @@
 # However, certain connections that you might not want to go through the client network will
 # Ex.: Downloading updates for Kali would go over client's local network (which might be flagged/blocked)
 
+# Variables
+LOCAL_GATEWAY=$(ip route | awk '/default/ {print $3}')
+
 # Reset to Clean/Default State
 sudo ip route del 0.0.0.0/1
 sudo ip route del 128.0.0.0/1
 sudo ip route del 10.0.0.0/8
 sudo ip route del 172.16.0.0/12
 sudo ip route del 192.168.0.0/16
-
-# This rule should always already exist (it is necessary for our connection to the NAB)
-sudo ip route add 10.8.0.0/24 dev tun0
 
 echo "Errors that say 'File exists/No such process' are okay. That just means a route change was redundant."
