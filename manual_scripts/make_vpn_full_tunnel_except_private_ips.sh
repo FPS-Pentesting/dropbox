07dfe624575162c726a5bdd56682bc2ccadd5357
jetbennett <wolleytej@gmail.com>
2025-12-03 11:21:24 -0500

    Update make_vpn_full_tunnel_except_private_ips.sh
    Updated with variable, and now using `dev tun0` instead of hardcoded VPN server IP.

--- manual_scripts/make_vpn_full_tunnel_except_private_ips.sh
+++ manual_scripts/make_vpn_full_tunnel_except_private_ips.sh
@@ -8,6 +8,9 @@
 # If the client has hosts in that range that need testing,
 # we will need to come up with a special solution (this has never happened)
 
+# Variables
+LOCAL_GATEWAY=$(ip route | awk '/default/ {print $3}')
+
 # Reset to Clean/Default State
 sudo ip route del 0.0.0.0/1
 sudo ip route del 128.0.0.0/1
@@ -16,16 +19,13 @@ sudo ip route del 172.16.0.0/12
 sudo ip route del 192.168.0.0/16
 
 # By default, route all traffic through the VPN tunnel
-sudo ip route add 128.0.0.0/1 via 10.8.0.1
-sudo ip route add 0.0.0.0/1 via 10.8.0.1
+sudo ip route add 128.0.0.0/1 dev tun0
+sudo ip route add 0.0.0.0/1 dev tun0
 
 # Route all private ip traffic through local eth0
-sudo ip route add 10.0.0.0/8 dev eth0
-sudo ip route add 172.16.0.0/12 dev eth0
-sudo ip route add 192.168.0.0/16 dev eth0
-
-# This rule should always already exist (it is necessary for our connection to the NAB)
-sudo ip route add 10.8.0.0/24 dev tun0
+sudo ip route add 10.0.0.0/8 via $LOCAL_GATEWAY
+sudo ip route add 172.16.0.0/12 via $LOCAL_GATEWAY
+sudo ip route add 192.168.0.0/16 dev $LOCAL_GATEWAY
 
 echo ""
 echo "Errors that say 'File exists/No such process' are okay. That just means a route change was redundant."
